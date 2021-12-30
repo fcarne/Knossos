bafa911441bfcfb19a2167f3e7fb6bddaf0ec5ff
Federico Carne <federico.carne@hotmail.com>
2021-12-30 08:44:45 +0100

    Eclipse new project

---
+++ src/Naraka.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Naraka.cpp
+// Author      : Federico Carne 1059865
+// Date        : 30/12/2021
+// Description : Main function
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
